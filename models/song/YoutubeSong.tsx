ac533274acc8275b555ffd1e53c6bc2f6c058900
Long <longnp@btc-studio.io>
2021-12-18 16:32:40 +0700

    first commit

---
+++ models/song/YoutubeSong.tsx
@@ -0,0 +1,13 @@
+class YoutubeSong {
+  id: string
+  title: string
+  thumbnail?: string
+
+  constructor(id: string, title: string, thumbnail?: string) {
+    this.id = id
+    this.title = title
+    this.thumbnail = thumbnail
+  }
+}
+
+export default YoutubeSong
